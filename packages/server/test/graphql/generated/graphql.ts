66b19d8b4dc3a29e14b1223ffb370079bf859ed0
Kristaps Fabians Geikins <fabis94@live.com>
2023-01-05 14:58:28 +0200

    feat: filtering out project models w/o versions

--- packages/server/test/graphql/generated/graphql.ts
+++ packages/server/test/graphql/generated/graphql.ts
@@ -1028,6 +1028,8 @@ export type ProjectCollection = {
 export type ProjectModelsFilter = {
   /** Filter by IDs of contributors who participated in models */
   contributors?: InputMaybe<Array<Scalars['String']>>;
+  /** Filter out models that don't have any versions */
+  onlyWithVersions?: InputMaybe<Scalars['Boolean']>;
   /** Filter by source apps used in models */
   sourceApps?: InputMaybe<Array<Scalars['String']>>;
 };
